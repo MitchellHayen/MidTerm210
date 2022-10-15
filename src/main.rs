ee9083385587ddf9f4aa5569e18ff3b800c86362
Mitch <mitchell.hayen@gmail.com>
2022-10-15 18:21:40 -0500

    added some comments

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::sync::{Arc, Mutex};
 
 fn main()
 {
+    //creating shared memory space protected by mutex
     let grid = Arc::new(Mutex::new([['-' as char; 4]; 4]));
 
     let mut characterArray = vec![];
@@ -19,17 +20,22 @@ fn main()
     characterArray.push(bomb2);
 
     let mut end = false;
+    //counters used for bomb movement
     let mut counter1 = 2;
     let mut counter2 = 0;
     let mut wizard_counter = 0;
     let mut wizard_shuffle = false;
     {
+        //bringing in our shared memory space
         let grid = Arc::clone(&grid);
+        //unlocking the shared memory space
         let mut grid1 = grid.lock().unwrap();
         grid1[gold1.0 as usize][gold1.1 as usize] = 'G';
         grid1[gold2.0 as usize][gold2.1 as usize] = 'G';
     }
+    //shared memory space is now locked again since it went out of scope
 
+    //main game loop
     loop
     {
         let bomb1_move = if counter1%4 == 0 {true} else {false};
@@ -51,32 +57,38 @@ fn main()
             }
             println!("Wizard Shuffle!");
             let mut gold_qty = false; //if false, no gold has been picked up so 2 gold need to be placed
+            //using character_array_clone -- if characterArray was used, it would be consumed and not usable again
             for mut character in character_array_clone
             {
                 let grid = Arc::clone(&grid);
+                //if robot has a gold piece, then we will only put 1 gold piece on the new board
                 if character.2 == 'R' && character.5 == true { gold_qty = true}
                 let mut rng = rand::thread_rng();
                 let mut row = rng.gen_range(0, 4);
                 let mut column = rng.gen_range(0, 4);
                 loop
                 {
+                    //changing position until an open space is found
                     let mut grid1 = grid.lock().unwrap();
                     row = rng.gen_range(0, 4);
                     column = rng.gen_range(0, 4);
                     if grid1[row as usize][column as usize] == '-' {break;}
                 }
+                //an open space was found, now we can change the characters coordinates
                 character.0 = row;
                 character.1 = column;
                 character.3 = row;
                 character.4 = column;
                 let mut grid1 = grid.lock().unwrap();
                 grid1[character.0 as usize][character.1 as usize] = character.2;
+                //add character to temp_character_array so that we can update characterArray later
                 temp_character_array.push(character);
             }
             let grid = Arc::clone(&grid);
             let mut rng = rand::thread_rng();
             let mut row = rng.gen_range(0, 4);
             let mut column = rng.gen_range(0, 4);
+            //gettings coords for gold piece
             loop
             {
                 let grid1 = grid.lock().unwrap();
@@ -86,9 +98,11 @@ fn main()
             }
 
             {
+                //setting gold piece but using a new scope so that the mutex locks when we're done editing it
                 let mut grid1 = grid.lock().unwrap();
                 grid1[row][column] = 'G';
             }
+            //if no gold pieces were picked up yet, look for place for another gold piece
             if !gold_qty
             {
                 let grid = Arc::clone(&grid);
@@ -107,18 +121,20 @@ fn main()
                 grid1[row][column] = 'G';
             }
 
-
+            //updating characterArray with the new positions of all characters
             characterArray = temp_character_array;
         }
+        //resetting so that the board doesn't shuffle every round
         wizard_shuffle = false;
 
-
+        //creating channels to bring information out of threads back to main thread to be used
         let (end_tx, end_rx) = mpsc::channel();
         let (wizard_tx, wizard_rx) = mpsc::channel();
         let mut handles = vec![];
         let mut tempCharacterArray = vec![];
         for mut character in characterArray.clone()
         {
+            //saving current coordinates to change current space to blank after character moves
             let prev_x = character.0 as usize;
             let prev_y = character.1 as usize;
             let mut temp_character = character;
@@ -126,6 +142,7 @@ fn main()
             let temp_end_tx = end_tx.clone();
             let temp_wizard_tx = wizard_tx.clone();
             let grid = Arc::clone(&grid);
+            //creating a thread for each character
             let handle = thread::spawn(move ||
                 {
                     let mut end1 = false;
@@ -137,11 +154,13 @@ fn main()
                         if character.2 == 'B' && character.5 == false && !bomb2_move { break };
                         if grid1[temp_character.0 as usize][temp_character.1 as usize] == '-'
                         {
+                            //found a place to move so we can update characters coordinates
                             character = temp_character;
                             break;
                         }
                         else if grid1[temp_character.0 as usize][temp_character.1 as usize] == 'G' && character.2 == 'R'
                         {
+                            //already has a gold piece
                             if character.5 == true
                             {
                                 println!("Robot wins!");
@@ -149,6 +168,7 @@ fn main()
                                 character = temp_character;
                                 break;
                             }
+                                //does not have any gold
                             else
                             {
                                 character = temp_character;
@@ -166,6 +186,7 @@ fn main()
                         }
                     }
                     let mut grid1 = grid.lock().unwrap();
+                    //sending information to be received in main thread
                     temp_end_tx.send(end1).unwrap();
                     temp_wizard_tx.send(wizard_shuffle).unwrap();
                     let temp_tx = tx.clone();
@@ -180,11 +201,13 @@ fn main()
 
         for handle in handles
         {
+            //wait for all threads to finish before moving on
             handle.join().unwrap();
         }
 
         characterArray = tempCharacterArray;
 
+        //Not all threads sent something, so we need to drop the ones that are still open
         drop(end_tx);
         for received in end_rx
         {
